17f5daf8eaa80efc95bbcab5a4950433d225b4a7
Ashrita Agarwal <ashritaagarwal.che18@iitbhu.ac.in>
2021-11-07 19:55:34 +0530

    Day 7

---
+++ day7-css/README.md
@@ -0,0 +1,5 @@
+# Day7 CSS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ashrita-ag/pen/GRvxPMd](https://codepen.io/ashrita-ag/pen/GRvxPMd).
+
+
---
+++ day7-css/script.js
@@ -0,0 +1,8 @@
+$(".search-icon").on("click", function () {
+	$(".search-input").toggleClass("active");
+});
+
+$(".icon").on("click", function () {
+	$(".front").toggleClass("click");
+	$(".back").toggleClass("click");
+});
