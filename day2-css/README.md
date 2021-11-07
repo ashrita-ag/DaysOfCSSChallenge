c516af00003fb2b1296e5eb57c77914cfde7688d
Ashrita Agarwal <ashritaagarwal.che18@iitbhu.ac.in>
2021-11-07 19:54:32 +0530

    Day 2

---
+++ day2-css/README.md
@@ -0,0 +1,5 @@
+# Day2 CSS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ashrita-ag/pen/WNEeZdR](https://codepen.io/ashrita-ag/pen/WNEeZdR).
+
+
---
+++ day2-css/script.js
@@ -0,0 +1,4 @@
+function handleclick() {
+	document.querySelector(".center").classList.toggle("animate");
+	document.querySelector(".center").classList.remove("no-animation");
+}
